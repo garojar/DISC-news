e7b84548a4bfc9cae89115c2136c3de56a760401
German Rojo Arce <garojar@hotmail.com>
2017-10-12 18:10:29 -0300

    Creacion de la clase NOticia y su prueba unitaria.
    * Se agrego libreria: AssertJ, Apache Commons Lang, Lombok.
    * Clase de prueba de noticia.

---
+++ DiscNews/app/src/main/java/cl/ucn/disc/dam/discnews/model/Noticia.java
@@ -0,0 +1,57 @@
+package cl.ucn.disc.dam.discnews.model;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+
+/**
+ * Representa una noticia en un instante de tiempo para el
+ * sistema de DISC News.
+ * @author Germán Rojo .
+ */
+
+public final class Noticia {
+    /**
+     * Descripcion en una linea del titulo de la noticia
+     */
+    @Getter
+    private String titulo="ELLIOT ESTA VIVOOOOOOOOOOOO";
+    /**
+     * Descripcion en 2 lineas y con un maximo de 140 caracteres
+     */
+    @Getter
+    private String resumen;
+
+    /**
+     * Contenido completo de la noticia
+     */
+    @Getter
+    private String contenido;
+
+    /**
+     * Fecha de la noticia
+     */
+    @Getter
+    private LocalDateTime  fecha;
+
+    /**
+     * Icono de la noticia de 64x64 pixels
+     */
+    @Getter
+    private String icono;
+
+    /**
+     * Autor de la noticia en formato: "German Rojo <garojar@hotmail.com>"
+     */
+    @Getter
+    private String autor;
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this,
+                ToStringStyle.MULTI_LINE_STYLE);
+    }
+}
